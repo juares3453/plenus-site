93852a935bcb222f0337374b6e8cdcfae879e2ee
juares3453 <juarescanalle@gmail.com>
2024-08-21 13:59:57 +0000

    about

--- resources/views/sections/menu.blade.php
+++ resources/views/sections/menu.blade.php
@@ -29,7 +29,7 @@
                     <a class="nav-link" href="#">|</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="quemsomos">QUEM SOMOS</a>
+                    <a class="nav-link" href="quem-somos">QUEM SOMOS</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">|</a>
